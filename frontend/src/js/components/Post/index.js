0ea1f3d92492451a038c0304deb5ba07d1bee249
Henry Pan <henrypoon1231@gmail.com>
2017-09-22 12:39:01 +1000

    update npm start

--- frontend/src/js/components/Post/index.js
+++ frontend/src/js/components/Post/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Card, CardMedia, CardTitle, CardText, CardActions } from 'react-toolbox/lib/card';
 import {Button} from 'react-toolbox/lib/button';
+import './Home.css';
 
 export default class Post extends React.Component {
 	constructor(props) {
@@ -13,8 +14,8 @@ export default class Post extends React.Component {
 	render() {	
 		const text = this.props.text;
 		return (
-			<div className='Post'>
-				<Card style={{width: '350px'}}>
+			<div className='Post_Root'>
+				<Card style={{ width: '350px' }}>
 					<CardTitle
 						avatar="https://placeimg.com/80/80/animals"
 						title="Avatar style title"
