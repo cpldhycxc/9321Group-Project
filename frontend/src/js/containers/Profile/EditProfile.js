7575612d582d95be61bf4bcf0419039f2b2a5293
Gary Chen <cpldhycxc@gmail.com>
2017-09-23 22:42:45 +1000

    inentation, import use single quote not double

--- frontend/src/js/containers/Profile/EditProfile.js
+++ frontend/src/js/containers/Profile/EditProfile.js
@@ -1,26 +1,26 @@
-import React from "react";
-import {Button, Media, Tab, Nav} from 'react-bootstrap';
+import React from 'react';
+import { Button, Media, Tab, Nav } from 'react-bootstrap';
 
 
 export default class EditProfile extends React.Component {
-	render(){
-		return(
+	render() {
+		return (
 			<div>
 			<form>
-  				<div className="form-group">
- 					 <label for ="email"> Email: </label>
- 					 <input type="email"  className="form-control" value="email" id="email"/>
- 				 </div>
- 				 <div className="form-group">
- 					 <label for="password"> New Password: </label>
-  					 <input type="password" className="form-control" id ="password"/>
-  				 </div>
-  				<div className="form-group">
- 					 <label for="confirm password"> New Password: </label>
- 					 <input type="password" className="form-control" id ="confirm password"/>
- 				</div>
-  				<input type="submit" value="Save Change"/>
-  				<input type="submit" value="Close"/>
+				<div className="form-group">
+					<label for="email"> Email: </label>
+					<input type="email" className="form-control" value="email" id="email" />
+				</div>
+				<div className="form-group">
+					<label for="password"> New Password: </label>
+					<input type="password" className="form-control" id ="password" />
+				</div>
+				<div className="form-group">
+					<label for="confirm password"> New Password: </label>
+					<input type="password" className="form-control" id ="confirm password" />
+				</div>
+				<input type="submit" value="Save Change" />
+				<input type="submit" value="Close" />
 			</form>
 			</div>
 		)
