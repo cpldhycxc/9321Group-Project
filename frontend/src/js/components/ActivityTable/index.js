b97621a7c8ac9c2ebe7eea0ff26e236fb94d16eb
Henry Pan <henrypoon1231@gmail.com>
2017-09-27 04:00:19 +1000

    completed activity report

--- frontend/src/js/components/ActivityTable/index.js
+++ frontend/src/js/components/ActivityTable/index.js
@@ -10,23 +10,26 @@ export default class ActivityTable extends React.Component {
 
 	render() {
 		return (
-		  <Table celled inverted selectable>
-		    <Table.Header>
-		      <Table.Row>
-		        <Table.HeaderCell>Date</Table.HeaderCell>
-		        <Table.HeaderCell>Activity</Table.HeaderCell>
-		      </Table.Row>
-		    </Table.Header>
-
-		    <Table.Body>
-		    	{this.props.data.activities.map((e, i) => (
-						<Table.Row>
-			        <Table.Cell>{e.data}</Table.Cell>
-			        <Table.Cell>{e.description}</Table.Cell>
-			      </Table.Row>
+			<Table celled inverted selectable>
+				<Table.Header>
+					<Table.Row>
+						<Table.HeaderCell>Date</Table.HeaderCell>
+						<Table.HeaderCell>Activity</Table.HeaderCell>
+					</Table.Row>
+				</Table.Header>
+				<Table.Body>
+					<Table.Row>
+						<Table.Cell>{this.props.data.joinDate}</Table.Cell>
+						<Table.Cell>You have joined UNSWBook!</Table.Cell>
+					</Table.Row>
+					{this.props.data.activities.map((e, i) => (
+						<Table.Row keu={i}>
+							<Table.Cell>{e.data}</Table.Cell>
+							<Table.Cell>{e.description}</Table.Cell>
+						</Table.Row>
 					))}
-		    </Table.Body>
-		  </Table>
+				</Table.Body>
+			</Table>
 		);
 	}
 }
