a93fa9fddb8355c3a4e5b1ae9291ac71c4601ba8
Gary Chen <cpldhycxc@gmail.com>
2017-09-28 00:02:54 +1000

    fuck

--- frontend/src/js/containers/Search/index.js
+++ frontend/src/js/containers/Search/index.js
@@ -1,37 +1,21 @@
 import React from 'react';
-import Select from 'react-select';
-import Dob from '../../components/asearch/DOB'
-import Gender from '../../components/asearch/gender'
+// import { connect } from "react-redux";
+import SearchBar from '../../components/SearchBar';
 
-class search extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			disabled: false,
-            isGender: false,
-            isDOB: false,
-			value: [],
-            gender: [],
-		};
-		this.selectChange = this.selectChange.bind(this);
-	}
-	selectChange(value) {
-		console.log('You selected: ', value);
-		this.setState({ value });
-	}
 
-	render() {
-		return (
-			<div className="section">
-			<h3 className="section-heading">Advance Search</h3>
-			<Dob />
-            <Gender />
-            <p></p>
-            <button >submit</button>
-			</div>
-		);
-	}
+export default class Search extends React.Component {
+  render() {
+    return (
+      <div className="front-page">
+          <div className='unswbook'>
+              UNSWBook
+          </div>
+          <div className='search-bar-int'>
+           <div className='input-group search-bar'>
+             <SearchBar />
+           </div>
+         </div>
+      </div>
+    );
+  }
 }
-
-
-export default search;
--- frontend/src/js/containers/adSearch/index.js
+++ frontend/src/js/containers/adSearch/index.js
@@ -1,25 +1,11 @@
 import React from 'react';
 import Select from 'react-select';
-
-const options = [
-  { value: 'one', label: 'One', gender: 'male' },
-  { value: 'two', label: 'Two', gender: 'male' },
-  { value: 'three', label: 'Three', gender: 'male' },
-  { value: 'four', label: 'Four', gender: 'male' },
-  { value: 'five', label: 'Five', gender: 'male' },
-  { value: 'six', label: 'Six', gender: 'male' }
-];
+import Dob from '../../components/asearch/DOB'
+import Gender from '../../components/asearch/gender'
 
 class search extends React.Component {
 	constructor(props) {
 		super(props);
-		this.state = {
-			disabled: false,
-            isGender: false,
-            isDOB: false,
-			value: [],
-            gender: [],
-		};
 		this.selectChange = this.selectChange.bind(this);
 	}
 	selectChange(value) {
@@ -31,26 +17,10 @@ class search extends React.Component {
 		return (
 			<div className="section">
 			<h3 className="section-heading">Advance Search</h3>
-			<Select
-				closeOnSelect={!this.state.stayOpen}
-				disabled={this.state.disabled}
-				multi
-				onChange={this.selectChange}
-				options={options}
-				placeholder="Please Select DOB"
-				simpleValue
-				value={this.state.value}
-			/>
-            <Select
-				closeOnSelect={!this.state.stayOpen}
-				disabled={this.state.disabled}
-				multi
-				onChange={this.selectChange}
-				options={options.gender}
-				placeholder="Please Select Gender"
-				simpleValue
-				value={this.state.gender}
-			/>
+			<Dob />
+            <Gender />
+            <p></p>
+            <button >submit</button>
 			</div>
 		);
 	}
