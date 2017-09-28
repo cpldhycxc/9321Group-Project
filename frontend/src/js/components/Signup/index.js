570cb87349709779efa63c3a4bd0824fec7114f8
Xubing Liang <liangxubing@gmail.com>
2017-09-28 13:54:58 +1000

    control login signup flow

--- frontend/src/js/components/Signup/index.js
+++ frontend/src/js/components/Signup/index.js
@@ -22,7 +22,8 @@ export default class SignupForm extends React.Component {
           passwordconfirm: '',
           birthday: moment().toISOString(),
           invalid: false,
-          errors: {}
+          errors: {},
+          issuccess: 2
         }
       this.onChange = this.onChange.bind(this);
       this.handleChange = this.handleChange.bind(this);
@@ -80,61 +81,84 @@ export default class SignupForm extends React.Component {
       if(this.isValid()){
         this.setState({ errors:{}, isloading:true });
         signuprequest(this.state).then((res)=>{
-
+          if(res.success){
+            this.setState({ issuccess: 1 });
+          } else {
+            this.setState({ issuccess: 0 });
+          }
         });
       }
     }
 
     render() {
-        const { errors } = this.state;
+        const { errors, issuccess } = this.state;
+        const result = ((issuccess) => {
+          if (issuccess == 1) return (
+            <div>
+              <Modal.Body>
+                <p>Signup successfully, please check your email to confirm</p>
+              </Modal.Body>
+            </div>);
+          if (issuccess == 0) return (
+            <div>
+              <Modal.Body>
+                <p>Fail to signup, try again later</p>
+              </Modal.Body>
+            </div>);
+          if (issuccess ==2 ) return (
+           <div>
+            <Modal.Body>
+            <form onSubmit={this.handleSubmit} >
+                <div className={classnames('form-group', {'has-error': errors.username})} >
+                    <label className="control-label">Username:</label>
+                    <input onBlur={this.checkUserExists} onChange={this.onChange} type="text" className="form-control" id ="username" name="username" placeholder="Enter Username" />
+                    {errors.username && <span className="help-block">{errors.username}</span>}
+                </div>
+                <div className ={classnames('form-group', {'has-error': errors.email})}>
+                    <label className="control-label">Email:</label>
+                    <input onChange={this.onChange} type="text" className="form-control" id ="email" name="email" placeholder="Enter email"/>
+                    {errors.email && <span className="help-block">{errors.email}</span>}
+                </div>
+                <div className ={classnames('form-group', {'has-error': errors.firstname})}>
+                    <label className="control-label">First Name: </label>
+                <input onChange={this.onChange} type="text" className="form-control" id ="firstname" name="firstname" placeholder="Enter your first name"/>
+                {errors.firstname && <span className="help-block">{errors.lastname}</span>}
+                </div>
+                <div className ={classnames('form-group', {'has-error': errors.lastname})}>
+                    <label className="control-label">Last Name: </label>
+                  <input onChange={this.onChange} type="text" className="form-control" id ="lastname" name="lastname" placeholder="Enter your last name"/>
+                  {errors.lastname && <span className="help-block">{errors.lastname}</span>}
+                </div>
+                <div className ='form-group'>
+                    <label className="control-label">Data of Birth: </label>
+                    <DatePicker id="example-datepicker" value={this.state.birthday} onChange={this.handleChange} />
+                </div>
+                <div className ={classnames('form-group', {'has-error': errors.password})}>
+                    <label className="control-label">Password: </label>
+                    <input onChange={this.onChange} type="password" className="form-control" id ="password" name="password" placeholder="Enter Password"/>
+                    {errors.password && <span className="help-block">{errors.password}</span>}
+                </div>
+                <div className ={classnames('form-group', {'has-error': errors.passwordconfirm})}>
+                    <label className="control-label">Confirm Password: </label>
+                <input onChange={this.onChange} type="password" className="form-control" id ="passwordconfirm" name="passwordconfirm" placeholder="Enter Password again"/>
+                {errors.passwordconfirm && <span className="help-block">{errors.passwordconfirm}</span>}
+                </div>
+                <div className="form-group">
+                    <Button onClick = {this.props.changeform} className='btn btn-primary btn-md backbutton' > Back</Button>
+                  <Button disabled={this.state.invalid} className='btn btn-primary btn-md' type="submit" name="Submit"> Signup</Button>
+                  {this.state.isloading ?
+                    <img src='static/images/loading.svg' height="50" width="50"/>:""}
+                </div>
+            </form>
+            </Modal.Body>
+          </div> );
+        }) (issuccess);
         return(
             <div>
                 <Modal.Header closeButton>
                     <Modal.Title>Signup</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                <form onSubmit={this.handleSubmit} >
-                    <div className={classnames('form-group', {'has-error': errors.username})} >
-                        <label className="control-label">Username:</label>
-                        <input onBlur={this.checkUserExists} onChange={this.onChange} type="text" className="form-control" id ="username" name="username" placeholder="Enter Username" />
-                        {errors.username && <span className="help-block">{errors.username}</span>}
-                    </div>
-                    <div className ={classnames('form-group', {'has-error': errors.email})}>
-                        <label className="control-label">Email:</label>
-                        <input onChange={this.onChange} type="text" className="form-control" id ="email" name="email" placeholder="Enter email"/>
-                        {errors.email && <span className="help-block">{errors.email}</span>}
-                    </div>
-                    <div className ={classnames('form-group', {'has-error': errors.firstname})}>
-                        <label className="control-label">First Name: </label>
-                    <input onChange={this.onChange} type="text" className="form-control" id ="firstname" name="firstname" placeholder="Enter your first name"/>
-                    {errors.firstname && <span className="help-block">{errors.lastname}</span>}
-                    </div>
-                    <div className ={classnames('form-group', {'has-error': errors.lastname})}>
-                        <label className="control-label">Last Name: </label>
-                      <input onChange={this.onChange} type="text" className="form-control" id ="lastname" name="lastname" placeholder="Enter your last name"/>
-                      {errors.lastname && <span className="help-block">{errors.lastname}</span>}
-                    </div>
-                    <div className ='form-group'>
-                        <label className="control-label">Data of Birth: </label>
-                        <DatePicker id="example-datepicker" value={this.state.birthday} onChange={this.handleChange} />
-                    </div>
-                    <div className ={classnames('form-group', {'has-error': errors.password})}>
-                        <label className="control-label">Password: </label>
-                        <input onChange={this.onChange} type="password" className="form-control" id ="password" name="password" placeholder="Enter Password"/>
-                        {errors.password && <span className="help-block">{errors.password}</span>}
-                    </div>
-                    <div className ={classnames('form-group', {'has-error': errors.passwordconfirm})}>
-                        <label className="control-label">Confirm Password: </label>
-                    <input onChange={this.onChange} type="password" className="form-control" id ="passwordconfirm" name="passwordconfirm" placeholder="Enter Password again"/>
-                    {errors.passwordconfirm && <span className="help-block">{errors.passwordconfirm}</span>}
-                    </div>
-                    <div className="form-group">
-                      <Button disabled={this.state.invalid} className='btn btn-primary btn-md' type="submit" name="Submit"> Signup</Button>
-                      {this.state.isloading ?
-                        <img src='static/images/loading.svg' height="50" width="50"/>:""}
-                    </div>
-                </form>
-                </Modal.Body>
+                {result}
             </div>
         );
     }
--- frontend/src/js/containers/User/index.js
+++ frontend/src/js/containers/User/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { searchByUsername } from '../../actions/userActions';
 // import { connect } from "react-redux";
 
 
 export default class UserProfile extends React.Component {
   componentWillMount () {
-     console.log(this.props.match.params);
+     const { username }= this.props.match.params;
+     console.log(username);
+     searchByUsername(username)
+     .then((res)=>{
+       console.log(res.data);
+     });
   }
 
 
