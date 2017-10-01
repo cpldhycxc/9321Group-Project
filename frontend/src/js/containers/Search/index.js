4fb86995087ed499c47fc527d1e66d6330e42516
Gary Chen <cpldhycxc@gmail.com>
2017-10-01 17:59:45 +1100

    fix

--- frontend/src/js/containers/Search/index.js
+++ frontend/src/js/containers/Search/index.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { Card, Icon } from 'semantic-ui-react';
+// import { connect } from "react-redux";
+import SearchBar from '../../components/SearchBar';
 
-const extra = (numOfFriend) => (
-  <a>
-    <Icon name='user' />
-    {numOfFriend} Friends
-  </a>
-);
-export default class ResultItem extends React.Component {
-	render() {
-		const id = this.props.id;
-		const numOfFriend = this.props.numOfFriend;
-		const self_intro = this.props.self_intro;
-		return (
-		<div class="card">
-			<Card
-				image='https://images.celebfamily.com/wp-content/uploads/2016/10/Kin-Jong-Un-Career.jpg'
-				meta={id}
-				description={self_intro}
-				extra={extra(numOfFriend)}
-			/>
-		</div>
-		);
-	}
-}
+export default class Search extends React.Component {
+    render() {
+        return (
+            <div className="front-page">
+                <div className='unswbook'>
+                    UNSWBook
+                </div>
+                <div className='search-bar-int'>
+                    <div className='input-group search-bar'>
+                        <SearchBar />
+                    </div>
+                </div>
+            </div>
+        );
+        
+    }
